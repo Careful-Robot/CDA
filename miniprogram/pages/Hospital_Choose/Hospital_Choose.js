4e9112800c51ddec723f3b35de6844bded23e4c5
Careful-Robot <fanjiaron68@hotmail.com>
2021-04-05 23:50:56 +0800

    Move Database to Cloud

--- miniprogram/pages/Hospital_Choose/Hospital_Choose.js
+++ miniprogram/pages/Hospital_Choose/Hospital_Choose.js
@@ -6,120 +6,31 @@ Page({
    */
   data: {
     type_id: 0,
-    type: [{
-      "name": "三甲",
-      "contain": [{
-        "id": 1,
-        "name": "珠海市香洲区第二人民医院"
-      }, {
-        "id": 2,
-        "name": "医院2"
-      }, {
-        "id": 3,
-        "name": "医院3"
-      }, {
-        "id": 4,
-        "name": "医院4"
-      }, {
-        "id": 5,
-        "name": "医院5"
-      }]
-    }, {
-      "name": "三乙",
-      "contain": [{
-        "id": 6,
-        "name": "医院6"
-      }, {
-        "id": 7,
-        "name": "医院7"
-      }, {
-        "id": 8,
-        "name": "医院8"
-      }, {
-        "id": 9,
-        "name": "医院9"
-      }, {
-        "id": 10,
-        "name": "医院10"
-      }]
-    }, {
-      "name": "三级",
-      "contain": [{
-        "id": 11,
-        "name": "医院11"
-      }, {
-        "id": 12,
-        "name": "医院12"
-      }, {
-        "id": 13,
-        "name": "医院13"
-      }, {
-        "id": 14,
-        "name": "医院14"
-      }, {
-        "id": 15,
-        "name": "医院15"
-      }]
-    }, {
-      "name": "二甲",
-      "contain": [{
-        "id": 16,
-        "name": "医院16"
-      }, {
-        "id": 17,
-        "name": "医院17"
-      }, {
-        "id": 18,
-        "name": "医院18"
-      }, {
-        "id": 19,
-        "name": "医院19"
-      }, {
-        "id": 20,
-        "name": "医院20"
-      }]
-    }, {
-      "name": "二乙",
-      "contain": [{
-        "id": 21,
-        "name": "医院21"
-      }, {
-        "id": 22,
-        "name": "医院22"
-      }, {
-        "id": 23,
-        "name": "医院23"
-      }, {
-        "id": 24,
-        "name": "医院24"
-      }, {
-        "id": 25,
-        "name": "医院25"
-      }]
-    }, {
-      "name": "其他",
-      "contain": [{
-        "id": 26,
-        "name": "医院26"
-      }, {
-        "id": 27,
-        "name": "医院27"
-      }, {
-        "id": 28,
-        "name": "医院28"
-      }, {
-        "id": 29,
-        "name": "医院29"
-      }, {
-        "id": 30,
-        "name": "医院30"
-      }]
-    }]
+    type: ["三甲", "三乙", "三级", "二甲", "二乙", "其他"]
   },
 
-  switchRightTab(id) {
+  getHospitalData(level = '三甲') {
+    wx.showLoading({
+      title: '正在加载中...',
+      mask: true
+    })
+    wx.cloud.callFunction({
+      name: 'getHospitalData',
+      data: {
+        level
+      }
+    }).then(res => {
+      this.setData({
+        hospital: res.result.data
+      })
+      wx.hideLoading()
+    })
+  },
+
+  switchRightTab(data) {
+    this.getHospitalData(data.currentTarget.dataset.level)
     this.setData({
-      type_id: id.currentTarget.dataset.id
+      type_id: data.currentTarget.dataset.id
     })
   },
 
@@ -127,7 +38,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.getHospitalData()
   },
 
   /**
