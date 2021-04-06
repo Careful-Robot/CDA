c5ad2cd30b6ab6021c630c54abc52b4ca7d48ada
Careful-Robot <fanjiaron68@hotmail.com>
2021-04-06 14:50:07 +0800

    Add function "getHospitalDataById"

---
+++ cloudfunctions/getHospitalDataById/index.js
@@ -0,0 +1,12 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+
+cloud.init({
+  env: 'carefulrobot-9gdwsth4e675d6ac'
+})
+const db = cloud.database()
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+  return await db.collection('Hospital').doc(event.id).get()
+}
