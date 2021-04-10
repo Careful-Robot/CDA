65d6b6bebb693c17b594e2cc1c6777426bfb935a
Careful-Robot <fanjiaron68@hotmail.com>
2021-04-11 00:32:19 +0800

    医生选择页面更新

--- miniprogram/pages/Doctor_Choose/Doctor_Choose.js
+++ miniprogram/pages/Doctor_Choose/Doctor_Choose.js
@@ -5,14 +5,56 @@ Page({
    * 页面的初始数据
    */
   data: {
+    date: []
+  },
 
+  getData(info) {
+    wx.showLoading({
+      title: '正在加载中...',
+      mask: true
+    })
+    wx.cloud.callFunction({
+      name: 'getDoctorData',
+      data: {
+        info
+      }
+    }).then(res => {
+      this.setData({
+        doctor: res.result.list
+      })
+      wx.cloud.callFunction({
+        name: 'getTimeData'
+      }).then(res1 => {
+        this.setData({
+          time: res1.result.data
+        })
+        wx.hideLoading()
+      })
+    })
+  },
+
+  getDate() {
+    let weekArr = ["周日", "周一", "周二", "周三", "周四", "周五", "周六"]
+    for (let index = 0; index < 7; index++) {
+      let date = new Date(new Date().getTime() + index * 86400000)
+      let month = date.getMonth() + 1
+      let day = date.getDate().toString()
+      let time = month + '.' + day
+      let week = weekArr[date.getDay()]
+      this.setData({
+        date: this.data.date.concat([
+          [week, time]
+        ])
+      })
+    }
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.getData(options)
+    this.getDate()
   },
 
   /**
