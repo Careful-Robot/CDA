63e9a2c3f4366dbc6ad3ff831a2f439a49820a71
Careful-Robot <fanjiaron68@hotmail.com>
2021-04-11 00:32:19 +0800

    添加测试页面（不做实际用途）

---
+++ miniprogram/pages/test/test.js
@@ -0,0 +1,89 @@
+// pages/test/test.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    // wx.cloud.callFunction({
+    //   name: 'dailyTimeSet',
+    // }).then(res => {
+    //   console.log(res)
+    // })
+
+
+    // const db = wx.cloud.database({
+    //   env: 'carefulrobot-9gdwsth4e675d6ac'
+    // })
+    // const _ = db.command
+    // var week = "monday"
+    // var temp_morning = `work_time.${week}_morning`
+    // var temp_afternoon = `work_time.${week}_afternoon`
+    // db.collection('Doctor')
+    //   .where(_.or({
+    //     [temp_morning]: true
+    //   }, {
+    //     [temp_afternoon]: true
+    //   })).get().then(res2 => {
+    //     res2.data.forEach(function (item, index) {
+    //       console.log(item)
+    //     })
+    //   })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
