2ec04d60c4ab616087bd2b839c769ae27af0b573
Careful-Robot <fanjiaron68@hotmail.com>
2021-04-12 00:07:57 +0800

    初步完成预约前端与后端

--- miniprogram/pages/Doctor_Choose/Doctor_Choose.js
+++ miniprogram/pages/Doctor_Choose/Doctor_Choose.js
@@ -22,28 +22,27 @@ Page({
       this.setData({
         doctor: res.result.list
       })
-      wx.cloud.callFunction({
-        name: 'getTimeData'
-      }).then(res1 => {
-        this.setData({
-          time: res1.result.data
-        })
-        wx.hideLoading()
-      })
+      wx.hideLoading()
     })
   },
 
   getDate() {
+    let datee = new Date()
+    var today = (datee.getTime() - datee.getHours() * 3600000 - datee.getMinutes() * 60000 - datee.getSeconds() * 1000).toString().slice(0, -3)
     let weekArr = ["周日", "周一", "周二", "周三", "周四", "周五", "周六"]
+    let weekArrEng = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"]
     for (let index = 0; index < 7; index++) {
       let date = new Date(new Date().getTime() + index * 86400000)
       let month = date.getMonth() + 1
       let day = date.getDate().toString()
       let time = month + '.' + day
-      let week = weekArr[date.getDay()]
+      let weekNum = date.getDay()
+      let week = weekArr[weekNum]
+      let weekEng = weekArrEng[weekNum]
+      let timee = (parseInt(today) + index * 86400).toString()
       this.setData({
         date: this.data.date.concat([
-          [week, time]
+          [week, time, weekEng, timee]
         ])
       })
     }
