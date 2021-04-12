ed737764df39a79fc207d5707898163948523faa
Careful-Robot <fanjiaron68@hotmail.com>
2021-04-12 23:51:14 +0800

    代码精简化

--- miniprogram/pages/Hospital_Choose/Hospital_Choose.js
+++ miniprogram/pages/Hospital_Choose/Hospital_Choose.js
@@ -1,14 +1,11 @@
-// miniprogram/pages/Hospital/Hospital.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
     level_id: 0,
     level: ["三甲", "三乙", "三级", "二甲", "二乙", "其他"]
   },
-
+  onLoad() {
+    this.getHospitalDataByLevel()
+  },
   getHospitalDataByLevel(level = '三甲') {
     wx.showLoading({
       title: '正在加载中...',
@@ -26,7 +23,6 @@ Page({
       wx.hideLoading()
     })
   },
-
   switchRightTab(data) {
     var level_id = data.currentTarget.dataset.id
     if (this.data.level_id !== level_id) {
@@ -35,61 +31,5 @@ Page({
         level_id
       })
     }
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    this.getHospitalDataByLevel()
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
   }
 })
