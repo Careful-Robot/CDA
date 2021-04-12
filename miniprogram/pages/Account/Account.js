e034017bd38df317554e84f3e09b427466e286c7
Careful-Robot <fanjiaron68@hotmail.com>
2021-04-13 00:10:36 +0800

    更新兼容性操作

--- miniprogram/pages/Account/Account.js
+++ miniprogram/pages/Account/Account.js
@@ -18,29 +18,37 @@ Page({
     }
   },
   getUserProfile() {
-    wx.getUserProfile({
-      desc: '用于完善个人资料',
-      success: (res) => {
-        wx.cloud.callFunction({
-          name: 'getUserProfile',
-          data: {
-            weRunData: wx.cloud.CloudID(res.cloudID),
-            obj: {
-              shareInfo: wx.cloud.CloudID(res.cloudID),
+    if (wx.getUserProfile) {
+      wx.getUserProfile({
+        desc: '用于完善个人资料',
+        success: (res) => {
+          wx.cloud.callFunction({
+            name: 'getUserProfile',
+            data: {
+              weRunData: wx.cloud.CloudID(res.cloudID),
+              obj: {
+                shareInfo: wx.cloud.CloudID(res.cloudID),
+              }
             }
-          }
-        }).then(res1 => {
-          this.setData({
-            userInfo: res.userInfo,
-            'userInfo.openId': res1.result.event.userInfo.openId,
-            hasUserInfo: true
+          }).then(res1 => {
+            this.setData({
+              userInfo: res.userInfo,
+              'userInfo.openId': res1.result.event.userInfo.openId,
+              hasUserInfo: true
+            })
+            getApp().globalData.userInfo = this.data.userInfo
+            getApp().globalData.hasUserInfo = true
+            this.setUserData(this.data.userInfo)
           })
-          getApp().globalData.userInfo = this.data.userInfo
-          getApp().globalData.hasUserInfo = true
-          this.setUserData(this.data.userInfo)
-        })
-      }
-    })
+        }
+      })
+    } else {
+      wx.showToast({
+        title: '微信版本过低，无法登陆！',
+        icon: 'none',
+        duration: 2000
+      })
+    }
   },
   setUserData(userInfo) {
     wx.cloud.callFunction({
