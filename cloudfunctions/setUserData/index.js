d964aa8d84c49543c0ddc763298b2eb0985bc5b6
Careful-Robot <fanjiaron68@hotmail.com>
2021-04-12 23:51:14 +0800

    添加云函数"setUserData"

---
+++ cloudfunctions/setUserData/index.js
@@ -0,0 +1,15 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+
+cloud.init({
+  env: 'carefulrobot-9gdwsth4e675d6ac'
+})
+const db = cloud.database()
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+  return await db.collection('User')
+    .add({
+      data: event.userInfo
+    })
+}
