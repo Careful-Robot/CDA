0970e674c80df0b5d9403a306a851849d6b00c83
Careful-Robot <fanjiaron68@hotmail.com>
2021-04-13 14:16:15 +0800

    新增加载提示

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -3,10 +3,13 @@ App({
     userInfo: {},
     hasUserInfo: false
   },
-  onLaunch() {
+  onLaunch() {    
     wx.cloud.init({
       env: 'carefulrobot-9gdwsth4e675d6ac',
       traceUser: true
+    })
+    wx.showLoading({
+      title: '加载中',
     })
     wx.cloud.callFunction({
       name: 'getUserProfile'
@@ -20,6 +23,7 @@ App({
         if (res.result.data.length != 0) {
           this.globalData.userInfo = res.result.data[0]
           this.globalData.hasUserInfo = true
+          wx.hideLoading()
           if (this.checkLoginReadyCallback) {
             this.checkLoginReadyCallback(res);
           }
