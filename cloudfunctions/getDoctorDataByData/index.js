168a3b4f447a3e547e2f15206fdaf3b09a39d457
Careful-Robot <fanjiaron68@hotmail.com>
2021-04-15 00:17:25 +0800

    添加云函数"getDoctorDataByData"

---
+++ cloudfunctions/getDoctorDataByData/index.js
@@ -0,0 +1,22 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+
+cloud.init({
+  env: 'carefulrobot-9gdwsth4e675d6ac'
+})
+const db = cloud.database()
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+  return await db.collection('Doctor').aggregate()
+    .lookup({
+      from: 'Hospital',
+      localField: 'hospital_id',
+      foreignField: '_id',
+      as: 'hospital',
+    })
+    .match({
+      hospital_id: event.info.id,
+      department: event.info.department
+    }).end()
+}
